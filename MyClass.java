0a8aa5b33b38551908461a64617ffc1fb39d6a66
Anushk <anushksanghvi@gmail.com>
2022-11-17 01:16:10 +0530

    Revert "Second commit"
    This reverts commit f992432f5e845509ab15f9fa1ad6c52d6d0cf927.

--- MyClass.java
+++ MyClass.java
@@ -7,17 +7,8 @@ public class MyClass{
         System.out.println("Multilication is:" + c);
     }
 
-    public void sum(int x, int y){
-        a=x;
-        b=y;
-        int d = a+b;
-        System.out.println("Sum is:" + d);
-    }
-
-
     public static void main(String args[]){
         MyClass ob = new MyClass();
         ob.mul(5, 10);
-        ob.sum(5, 10);
     }
 }
