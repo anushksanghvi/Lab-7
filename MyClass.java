070cb7da8cd5a33f2b6ab9c31a95202eb0b448da
Anushk <anushksanghvi@gmail.com>
2022-11-17 00:06:18 +0530

    First commit

---
+++ MyClass.java
@@ -0,0 +1,14 @@
+public class MyClass{
+    int a, b;
+    public void mul(int x, int y){
+        a=x;
+        b=y;
+        int c = a*b;
+        System.out.println("Multilication is:" + c);
+    }
+
+    public static void main(String args[]){
+        MyClass ob = new MyClass();
+        ob.mul(5, 10);
+    }
+}
